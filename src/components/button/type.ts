94a4d665ea2c4deddc5f50c2905f8c71e7b2eb41
biksbee <77857837+biksbee@users.noreply.github.com>
2023-08-24 12:15:37 +0300

    Delete src directory

--- src/components/button/type.ts
+++
@@ -1,6 +0,0 @@
-export type ButtonType = 'active' | 'disabled'
-export const ButtonState:Record<ButtonType, string> = {
-    active: "bg-c_yellow text-[#000] cursor-pointer hover:opacity-80 duration-500",
-    disabled: "bg-c_grey-accent text-c_grey-disabled"
-}
-
